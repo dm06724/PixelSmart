08a7e8fbe2cf1a9be24c06cbd56dec5819969f6e
hadley31 <nick.hadley31@gmail.com>
2019-11-16 14:21:07 -0500

    fixed minor issues

--- src/pixelsmart/tools/EraserTool.java
+++ src/pixelsmart/tools/EraserTool.java
@@ -6,7 +6,6 @@ import java.awt.Graphics2D;
 import java.awt.geom.Path2D;
 import java.awt.image.BufferedImage;
 
-import pixelsmart.Input;
 import pixelsmart.commands.CommandList;
 import pixelsmart.commands.UpdateLayerDataCommand;
 import pixelsmart.image.Image;
@@ -18,13 +17,13 @@ public class EraserTool extends DrawingTool {
 	@Override
 	public void startAction(Image image) {
 		finalStrokeShape = new Path2D.Double();
-		finalStrokeShape.moveTo(Input.getMouseX(), Input.getMouseY());
+		finalStrokeShape.moveTo(image.getActiveLayer().getMouseX(), image.getActiveLayer().getMouseY());
 	}
 
 	@Override
 	public void updateAction(Image image) {
-		int mx = Input.getMouseX();
-		int my = Input.getMouseY();
+		int mx = image.getActiveLayer().getMouseX();
+		int my = image.getActiveLayer().getMouseY();
 
 		finalStrokeShape.lineTo(mx, my);
 	}
--- src/pixelsmart/tools/LineTool.java
+++ src/pixelsmart/tools/LineTool.java
@@ -1,8 +1,10 @@
 package pixelsmart.tools;
 
+import java.awt.BasicStroke;
 import java.awt.Graphics2D;
 import java.awt.image.BufferedImage;
 
+import pixelsmart.Project;
 import pixelsmart.commands.CommandList;
 import pixelsmart.commands.UpdateLayerDataCommand;
 import pixelsmart.image.Image;
@@ -28,7 +30,12 @@ public class LineTool extends DrawingTool {
 
         BufferedImage newData = layer.copyData();
 
-        Graphics2D g = layer.getData().createGraphics();
+        Graphics2D g = newData.createGraphics();
+
+        g.setColor(Project.getCurrent().getPrimaryBrushColor());
+        BasicStroke stroke = new BasicStroke(getBrushSize(), BasicStroke.CAP_ROUND, BasicStroke.JOIN_ROUND);
+        g.setStroke(stroke);
+
         g.drawLine(startMX, startMY, mx, my);
         g.dispose();
 

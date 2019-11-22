41e6143a2733fe36080ce336fb7793811c418399
hadley31 <nick.hadley31@gmail.com>
2019-11-22 00:13:46 -0500

    implemented move tool snapping

--- src/pixelsmart/tools/MoveTool.java
+++ src/pixelsmart/tools/MoveTool.java
@@ -6,7 +6,7 @@ import pixelsmart.image.Layer;
 import pixelsmart.ui.ImagePanel;
 
 public class MoveTool extends ToolAdapter {
-    private static final int SNAP_DISTANCE = 15;
+    private static final int MAX_SNAP_DISTANCE = 5;
 
     private int mouseOffsetX, mouseOffsetY;
 
@@ -24,41 +24,85 @@ public class MoveTool extends ToolAdapter {
     public void updateAction(final ImagePanel panel) {
         final Layer layer = panel.getActiveLayer();
 
+        // TODO: Snaps to other layers well, but does not snap to image.
+        // Just need to check with 0,0 and image.getWidth() and image.getHeight()
+        // Draw purple lines where it is wanting to snap to
+
         final int lx = panel.getMouseX(ImagePanel.RELATIVE_TO_IMAGE) + mouseOffsetX;
         final int ly = panel.getMouseY(ImagePanel.RELATIVE_TO_IMAGE) + mouseOffsetY;
-
-        // TODO Snapping
-        // Rectangle rect = panel.getLayerRect(layer);
-        // int closestX = Integer.MAX_VALUE, closestY = Integer.MAX_VALUE;
-        // int x = lx, y = ly;
-        // for (Layer l : panel.getImage()) {
-        // Rectangle otherRect = panel.getLayerRect(l);
-        // int dist;
-        // if ((dist = Math.abs(lx - otherRect.x)) < closestX && dist < SNAP_DISTANCE) {
-        // x = otherRect.x;
-        // closestX = dist;
-        // } else if ((dist = Math.abs((lx + rect.width) - (otherRect.x +
-        // otherRect.width))) < closestX
-        // && dist < SNAP_DISTANCE) {
-        // x = otherRect.x + otherRect.width - rect.width;
-        // closestX = dist;
-        // }
-        // if ((dist = Math.abs(ly - otherRect.y)) < closestY && dist < SNAP_DISTANCE) {
-        // y = otherRect.x;
-        // closestY = dist;
-        // } else if ((dist = Math.abs((ly + rect.height) - (otherRect.y +
-        // otherRect.height))) < closestY
-        // && dist < SNAP_DISTANCE) {
-        // y = otherRect.y + otherRect.height - rect.height;
-        // closestY = dist;
-        // }
-        // }
 
         int snapX = lx;
         int snapY = ly;
 
-        for (Layer l : panel.getImage()){
-            
+        final Rectangle rect = layer.getRect();
+        final int maxX = lx + rect.width;
+        final int maxY = ly + rect.height;
+        int dist;
+        int closestX = Integer.MAX_VALUE;
+        int closestY = Integer.MAX_VALUE;
+        for (Layer l : panel.getImage()) {
+            if (layer == l)
+                continue;
+
+            final Rectangle otherRect = l.getRect();
+            final int otherMaxX = otherRect.x + otherRect.width;
+            final int otherMaxY = otherRect.y + otherRect.height;
+
+            // Test X, X
+            dist = Math.abs(lx - otherRect.x);
+            if (dist <= MAX_SNAP_DISTANCE && dist < closestX) {
+                snapX = otherRect.x;
+                closestX = dist;
+            }
+
+            // Test X, Width
+            dist = Math.abs(lx - otherMaxX);
+            if (dist <= MAX_SNAP_DISTANCE && dist < closestX) {
+                snapX = otherMaxX;
+                closestX = dist;
+            }
+
+            // Test Max X, X
+            dist = Math.abs(maxX - otherRect.x);
+            if (dist <= MAX_SNAP_DISTANCE && dist < closestX) {
+                snapX = otherRect.x - rect.width;
+                closestX = dist;
+            }
+
+            // Test Width, Width
+            dist = Math.abs(maxX - otherMaxX);
+            if (dist <= MAX_SNAP_DISTANCE && dist < closestX) {
+                snapX = otherMaxX - rect.width;
+                closestX = dist;
+            }
+
+            // Test Y, Y
+            dist = Math.abs(ly - otherRect.y);
+            if (dist <= MAX_SNAP_DISTANCE && dist < closestY) {
+                snapY = otherRect.y;
+                closestY = dist;
+            }
+
+            // Test Y, Height
+            dist = Math.abs(ly - otherMaxY);
+            if (dist <= MAX_SNAP_DISTANCE && dist < closestY) {
+                snapY = otherMaxY;
+                closestY = dist;
+            }
+
+            // Test Height, Y
+            dist = Math.abs(maxY - otherRect.y);
+            if (dist <= MAX_SNAP_DISTANCE && dist < closestY) {
+                snapY = otherRect.y - rect.height;
+                closestY = dist;
+            }
+
+            // Test Height, Height
+            dist = Math.abs(maxY - otherMaxY);
+            if (dist <= MAX_SNAP_DISTANCE && dist < closestY) {
+                snapY = otherMaxY - rect.width;
+                closestY = dist;
+            }
         }
 
         layer.setPosition(snapX, snapY);
